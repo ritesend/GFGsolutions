dd34f86445169dc53f1aada898515f5898ef9341
ritesend <101635863+ritesend@users.noreply.github.com>
2024-04-24 22:59:04 +0530

    Add files via upload

---
+++ Minimize_the_Heights_II.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    int getMinDiff(int arr[], int n, int k) {
+
+        if (n==1){
+            return 0;
+        }
+
+        sort(arr,arr+n);
+
+        int diff = arr[n-1] - arr[0];  //Max difference it can have
+        int maxi,mini;
+
+        for (int i = 1;i<n;i++){
+            if (arr[i] - k < 0){
+                continue;
+            }
+
+            maxi = max(arr[i-1]+k,arr[n-1]-k);
+            mini = min(arr[0]+k,arr[i]-k);
+
+            diff = min(diff,maxi-mini);
+        }
+        return diff;
+
+    }
+};
