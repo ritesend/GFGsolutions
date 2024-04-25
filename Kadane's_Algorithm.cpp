2f9d4b60b28f64d003cb79e41ff9ba200fa49d15
ritesend <101635863+ritesend@users.noreply.github.com>
2024-04-26 00:22:38 +0530

    Add files via upload

---
+++ Kadane's_Algorithm.cpp
@@ -0,0 +1,26 @@
+class Solution{
+public:
+    // arr: input array
+    // n: size of array
+    //Function to find the sum of contiguous subarray with maximum sum.
+    long long maxSubarraySum(int arr[], int n){
+
+        long long sum = 0,maxi = LONG_MIN;
+        for (int i = 0; i < n;i++){
+
+            sum += arr[i];
+
+            if (sum > maxi){
+                maxi = sum;
+            }
+
+            if (sum < 0){
+                sum = 0;
+            }
+
+        }
+        return maxi;
+
+
+    }
+};
